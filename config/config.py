786425b4e642fa5ebe99385a11ef715931f90401
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-08-29 18:15:06 -0500

    gitup

--- config/config.py
+++ config/config.py
@@ -1,4 +1,7 @@
+# coding: utf-8
 import os
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0,parentdir)
 
 CONFIG = {
     'FACEBOOK_TOKEN': os.env['access_token'],
