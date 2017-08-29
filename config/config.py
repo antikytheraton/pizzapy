236f12f5a732301772da413f2979592302f96a49
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-08-29 18:17:45 -0500

    gitup

--- config/config.py
+++ config/config.py
@@ -1,10 +1,8 @@
 # coding: utf-8
 import os
-parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.sys.path.insert(0,parentdir)
 
 CONFIG = {
-    'FACEBOOK_TOKEN': os.env['access_token'],
-    'VERIFY_TOKEN': os.env['verify_token'],
+    'FACEBOOK_TOKEN': os.environ['access_token'],
+    'VERIFY_TOKEN': os.environ['verify_token'],
     'SERVER_URL': 'https://pizza-bootbot.herokuapp.com'
 }
